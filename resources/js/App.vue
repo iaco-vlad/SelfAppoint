e4914f3b2dcd42ef77752cb1091887672c3ac98c
Vlad Iacovenco <iaco.vlad@gmail.com>
2023-02-10 14:01:15 +0200

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1> Hello, Vuejs with Laravel </h1>
+</template>
+
+<script>
+export default {
+        mounted() {
+            console.log('Component mounted.')
+    }
+}
+</script>
