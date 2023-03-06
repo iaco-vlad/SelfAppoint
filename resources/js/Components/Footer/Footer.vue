34fe4ffa115696d37ab89b95a1e57176b86d8cb1
Vlad Iacovenco <iaco.vlad@gmail.com>
2023-03-06 16:22:00 +0200

    Improve readme.

--- resources/js/Components/Footer/Footer.vue
+++ resources/js/Components/Footer/Footer.vue
@@ -5,7 +5,12 @@
                 <div class="col-sm-6">
                     &copy; {{ currentYear }} by {{ owner }}. All rights reserved.
                 </div>
+
                 <div class="col-sm-6 text-right">
+                    <a :href="technicalInformationLink" target="_blank">Technical information about the project</a>
+
+                    <span> | </span>
+
                     <a :href="ownerLink" target="_blank">{{ owner }}</a>
                 </div>
             </div>
@@ -20,6 +25,7 @@ export default {
         return {
             owner: 'Vlad Iacovenco',
             ownerLink: 'https://www.vlad-iacovenco.com',
+            technicalInformationLink: 'https://iacovlad.notion.site/SelfAppoint-c3e54c3558bd428bbecfc1da3a6a5868',
             currentYear: new Date().getFullYear()
         };
     }
